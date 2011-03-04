13b6a8c1ec2274c54842640b17598be30ba573d7
Ayelet Dahan <anativ@gmail.com>
2011-03-04 18:05:02 +0200

    some basic cleanup to the code

---
+++ ColorSampler/yaelet/utils/ColorCalculator/LoadColorFiles.java
@@ -0,0 +1,54 @@
+package yaelet.utils.ColorCalculator;
+
+import java.lang.String;
+import java.util.TreeMap;
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class LoadColorFiles {
+
+
+	private static File[] getFiles(String Path)
+	{
+		File FilesDir = new File(Path);
+		
+		FilenameFilter filterGetImages = new FilenameFilter() {
+		    public boolean accept(File dir, String name) {
+		        return name.endsWith(".jpg");
+		    }
+		};
+
+		File[] FilesList = FilesDir.listFiles(filterGetImages);
+		
+		
+		return FilesList;
+		
+	}
+	
+	public static TreeMap<Integer, String> getColorsFromDir(String Path)
+	{
+		TreeMap<Integer, String> returnValue = new TreeMap<Integer, String>();
+		
+		for (File ImageFile : getFiles(Path)) {
+			int key = Calculator.getTopColor(ImageFile);
+			if (!returnValue.containsKey(key))
+			{
+				returnValue.put(key, ImageFile.getName());
+			}
+			else
+			{
+				System.out.format("Color %s already exists. Skipping.", Integer.toHexString(key));
+			}
+		}
+		
+		return returnValue;
+	}
+	
+	public static int getColorsFromFile(String Path)
+	{
+		File ImageFile = new File(Path);
+
+		int key = Calculator.getTopColor(ImageFile);
+		return key;
+	}
+}
--- ColorSampler/yaelet/utils/ColorCalculator/TestCalculator.java
+++ ColorSampler/yaelet/utils/ColorCalculator/TestCalculator.java
@@ -2,17 +2,19 @@ package yaelet.utils.ColorCalculator;
 import java.io.File;
 import java.lang.String;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class TestCalculator {
 
-	
+	private static final String CompanyColorFilesPath = "C:\\Users\\Eyal\\Workspace\\MyColorFinder\\Samples";
+	private static final String TestFilesPath = "C:\\Users\\Eyal\\Workspace\\MyColorFinder\\Samples\\Testers";
 	
 	public static void main(String[] args)
 	{
 		
 		// get the makeup colors from the sample images
-		TreeMap<Integer, String> colors = LoadMakeupColors.getAvonColors();
+		TreeMap<Integer, String> colors = LoadColorFiles.getColorsFromDir(CompanyColorFilesPath);
 		
 		// DEBUG: print the colors Hash table
 		Iterator<Integer> color = colors.keySet().iterator();
@@ -23,31 +25,13 @@ public class TestCalculator {
 		}
 		
 		// get the color from a tester file
-		String testFile = "C:\\Users\\Eyal\\Workspace\\MyColorFinder\\Samples\\Testers\\Eyal.jpg";
-		int testColor = Calculator.getTopColor(new File(testFile));
-
+		int FindMyColor = LoadColorFiles.getColorsFromFile(TestFilesPath);
 		//DEBUG: print the color found in the sample
-		System.out.format("test color is %s%n", Integer.toHexString(testColor));
+		System.out.format("test color is %s%n", Integer.toHexString(FindMyColor));
 
+		Map.Entry<Integer, String> MyMatch = colors.higherEntry(FindMyColor);
 		// the closest product color
-		System.out.format("The relevant product is:%s%n", colors.higherEntry(testColor).getValue());
+		System.out.format("The relevant product is:%s%n", MyMatch.getValue());
 		
-		
-		/*
-		Image image = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Eyal\\Pictures\\6AM.jpg");
-		//Image image = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Eyal\\Pictures\\com.extjs.ext.2.2.gif");
-		Calculator calc = new Calculator(image);
-		try {
-			if (calc.Init())
-			{
-				//calc.Calculate();
-				Calculator.printInverseHistogram(Calculator.BuildHistogram(calc.GetImagesArray()));
-			}
-
-		} catch (Exception e) {
-			System.out.println(String.format("Exception thrown with message %s", 
-					e.getMessage()));
-		}
-		*/
 	}
 }
